fc794bee6756bf2ddfd85c67c393de2b68fdc40d
Chase Allen <ccallen001@gmail.com>
2022-12-10 22:42:41 -0500

    add and refactor tests

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,17 +7,41 @@ blogsRouter.get('/', async (_, res) => {
   res.json(blogs);
 });
 
+blogsRouter.get('/:id', async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+
+  blog ? res.json(blog) : res.send(404).end();
+});
+
 blogsRouter.post('/', async (req, res) => {
   const { body } = req;
   const { title, url, likes } = body;
 
-  if (!title || !url) return res.status(400).send();
+  if (!title || !url) return res.status(400).send('title and url are required');
 
   if (likes === undefined) body.likes = 0;
 
   const blog = await new Blog(body).save();
 
-  blog ? res.status(201).json(blog) : res.status(400).send();
+  blog ? res.status(201).json(blog) : res.status(400).end();
+});
+
+blogsRouter.put('/:id', async (req, res) => {
+  console.log(req.body);
+
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    context: 'query'
+  });
+
+  res.json(updatedBlog);
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+
+  response.status(204).end();
 });
 
 module.exports = blogsRouter;
--- tests/list-api.test.js
+++ tests/list-api.test.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const blog = require('../models/blog');
 const api = supertest(app);
 
 const { Blog, initialBlogs } = require('./test-helper');
@@ -13,78 +14,118 @@ beforeEach(async () => {
   }
 });
 
-test('blogs are returned as json', async () =>
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/));
+describe('getting blogs', () => {
+  test('blogs are returned as json', async () =>
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/));
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs');
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
 
-  expect(response.body).toHaveLength(initialBlogs.length);
+    expect(response.body).toHaveLength(initialBlogs.length);
+  });
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs');
+
+    const titles = response.body.map((blog) => blog.title);
+
+    expect(titles).toContain(initialBlogs[1].title);
+  });
 });
 
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs');
+describe('posting a blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'New Blog',
+      author: 'New Blog Author',
+      url: 'https://www.new-blog.com',
+      likes: 5
+    };
 
-  const titles = response.body.map((blog) => blog.title);
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
-  expect(titles).toContain(initialBlogs[1].title);
+    const response = await api.get('/api/blogs');
+
+    const titles = response.body.map((resp) => resp.title);
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1);
+
+    expect(titles).toContain(newBlog.title);
+  });
+
+  test("unique identifier '_id' is transformed to 'id' in Schema and is defined in response", async () => {
+    const { body } = await api.get('/api/blogs');
+
+    expect(body.length).toEqual(initialBlogs.length);
+
+    for (const blog of body) {
+      expect(blog.id).toBeDefined();
+    }
+  });
+
+  test('if likes are missing, likes default to 0', async () => {
+    const newBlog = {
+      title: 'Missing Likes',
+      author: 'A Guy Without Likes',
+      url: 'likes@undefined.com'
+    };
+
+    const { body } = await api.post('/api/blogs').send(newBlog);
+
+    expect(body.likes).toEqual(0);
+  });
+
+  test('if title or url are missing, response is 400', async () => {
+    const newBlog = {
+      author: 'A Guy With No Name',
+      likes: 10
+    };
+
+    await api.post('/api/blogs').send(newBlog).expect(400);
+  });
 });
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'New Blog',
-    author: 'New Blog Author',
-    url: 'https://www.new-blog.com',
-    likes: 5
-  };
+describe('updating (PUT) a blog', () => {
+  test('a blog can be updated', async () => {
+    const resp = await api.get('/api/blogs');
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    const { id, ...blog } = resp.body[0];
 
-  const response = await api.get('/api/blogs');
+    const updatedLikes = 10;
 
-  const titles = response.body.map((resp) => resp.title);
+    await api
+      .put(`/api/blogs/${id}`)
+      .send({ ...blog, title: 'Updated', likes: updatedLikes });
 
-  expect(response.body).toHaveLength(initialBlogs.length + 1);
+    const updatedBlog = await Blog.findById(id);
 
-  expect(titles).toContain(newBlog.title);
+    expect(updatedBlog.title).toEqual('Updated');
+
+    expect(updatedBlog.likes).toEqual(updatedLikes);
+  });
 });
 
-test("unique identifier '_id' is transformed to 'id' in Schema and is defined in response", async () => {
-  const { body } = await api.get('/api/blogs');
+describe('deleting a blog', () => {
+  test('a blog can be deleted', async () => {
+    const blogs = await api.get('/api/blogs');
 
-  expect(body.length).toEqual(initialBlogs.length);
+    const { id } = blogs.body[0];
 
-  for (blog of body) {
-    expect(blog.id).toBeDefined();
-  }
-});
+    await api.delete(`/api/blogs/${id}`);
 
-test('if likes are missing, likes default to 0', async () => {
-  const newBlog = {
-    title: 'Missing Likes',
-    author: 'A Guy Without Likes',
-    url: 'likes@undefined.com'
-  };
+    const blogsOneLess = await api.get('/api/blogs');
 
-  const { body } = await api.post('/api/blogs').send(newBlog);
+    expect(blogsOneLess.body).toHaveLength(initialBlogs.length - 1);
 
-  expect(body.likes).toEqual(0);
-});
-
-test('if title or url are missing, response is 400', async () => {
-  const newBlog = {
-    author: 'A Guy With No Name',
-    likes: 10
-  };
-
-  await api.post('/api/blogs').send(newBlog).expect(400);
+    expect(blogsOneLess.body.map((blog) => blog.id)).not.toContain(id);
+  });
 });
 
 afterAll(() => mongoose.connection.close());
